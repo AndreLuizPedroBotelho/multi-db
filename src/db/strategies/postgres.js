4407faad66a011bf537daab80538d6a069d997cc
dede-pc <andre-dede96@hotmail.com>
2019-04-08 19:18:40 -0300

    Primeiro Commit

---
+++ src/db/strategies/postgres.js
@@ -0,0 +1,75 @@
+const ICrud = require('./interfaces/interfaceCrud')
+const Sequelize = require('sequelize')
+
+class Postgres extends ICrud {
+    constructor() {
+        super()
+        this._driver = null
+        this._herois = null
+    }
+
+    async isConnected() {
+        try {
+            await this._driver.autheticated;
+            return true;
+
+        } catch (error) {
+            console.error('fail', error);
+            return false;
+        }
+    }
+
+    async defineModal() {
+        this._herois = this._driver.define('tb_herois', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nome: {
+                type: Sequelize.STRING,
+                required: true,
+            },
+            poder: {
+                type: Sequelize.STRING,
+                required: true,
+            }
+        }, {
+                tableName: 'tb_herois',
+                freezeTableName: false,
+                timestamps: false
+            })
+        await this._herois.sync()
+    }
+
+    async create(item) {
+        const {
+            dataValues
+        } = await this._herois.create(item)
+
+        return dataValues
+    }
+
+    async read(item) {
+        return this._herois.findAll({ where: item, raw: true })
+    }
+
+    async connect() {
+        this._driver = new Sequelize(
+            'tb_herois',
+            'postgres',
+            'Postgres2019!',
+            {
+                host: 'localhost',
+                port: 15432,
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorsAliases: false
+            }
+        )
+        this.defineModal()
+    }
+}
+
+module.exports = Postgres
